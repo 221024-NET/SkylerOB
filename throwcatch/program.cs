9571563203d8a3c7daed505ead1fcf131dd1b0cc
SkylerOB <skyler017@revature.net>
2022-10-26 14:59:10 -0700

    added findException

--- throwcatch/program.cs
+++ throwcatch/program.cs
@@ -10,38 +10,58 @@ public class Program
 	// [access modifier] [modifier] [retu type] [method name] ([parameters])
 	public static void Main()
 	{
-        // throw errors in subroutines, catch in main
+        // Let the games begin
 		Program ThCa = new Program();
 
+        
         // handle the exception where it occurs
         ThCa.handledException();
         // nothing to do here
-
 
         // Let the exception occur and deal with it here.
         try
         {
            ThCa.unhandledException();
         }
-        catch (IndexOutOfRangeException a)
+        catch //(IndexOutOfRangeException a)
         {
-            Console.WriteLine("ArrayError triggered-" + a);
+            //Console.WriteLine("ArrayError triggered-" + a);
+            Console.WriteLine("ArrayError triggered-");
         }
         finally
         {
-            Console.WriteLine("Message that prints regardless.");
+            Console.WriteLine("Message that prints regardless from main.");
+        }
+        
+
+        // identify the exception where it occurs but send a different one here
+        try
+        {
+            ThCa.findmetheException();
+        }
+        catch (IndexOutOfRangeException a)
+        {
+            Console.WriteLine("ArrayError should not have been triggered here-"+a);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("A message from the error-" + e);
+        }
+        finally
+        {
+            Console.WriteLine("Message that prints regardless from main.");
         }
         
         
         Console.WriteLine("End of Program. Exiting.");
-
+        // eop
     }
     
     void handledException()
     {
-        int[] sequence = new int[4];
+        int[] sequence = new int[2];
         int digit = sequence.Length;
-        Console.WriteLine("Array size is: " + digit);
+        Console.WriteLine("Entering handled. Array size is: " + digit);
         Console.WriteLine("There cannot be an element at index " + digit);
 
         for(int i=0; i<=digit; i++)
@@ -51,27 +71,59 @@ public class Program
                 sequence[i] = i;
                 Console.WriteLine(i + ": " + sequence[i] + "     Nice");
             }
-            catch (IndexOutOfRangeException a)
+            catch //(IndexOutOfRangeException a)
             {
-                Console.WriteLine("ArrayError triggered at " + i + ":" + a);
+                //Console.WriteLine("ArrayError triggered at " + i + ":" + a);
+                Console.WriteLine("ArrayError triggered at " + i);
             }
             finally
             {
-                Console.WriteLine("Message that prints regardless.");
+                Console.WriteLine("Message that prints regardless from handled.");
             }
         }
+        Console.WriteLine("Leaving handled");
     }
 
     void unhandledException()
     {
-        int[] sequence = new int[5];
+        int[] sequence = new int[2];
         int digit = sequence.Length;
-        Console.WriteLine("Array size is: " + digit);
+        Console.WriteLine("Entering unhandled. Array size is: " + digit);
         Console.WriteLine("There cannot be an element at index " + digit);
         for(int i=0; i<=digit; i++)
         {
             sequence[i] = i;
             Console.WriteLine(i + ": " + sequence[i]);
         }
+        Console.WriteLine("Leaving unhandled (should never print)");
+    }
+
+    void findmetheException()
+    {
+        int[] sequence = new int[2];
+        int digit = sequence.Length;
+        Console.WriteLine("Entering find. Array size is: " + digit);
+        Console.WriteLine("There cannot be an element at index " + digit);
+
+        for(int i=0; i<=digit; i++)
+        {
+            try
+            {
+                sequence[i] = i;
+                Console.WriteLine(i + ": " + sequence[i] + "     Nice");
+            }
+            catch //(IndexOutOfRangeException a)
+            {
+                //Console.WriteLine("ArrayError triggered at "+i+". "+a+". You deal with it.");
+                //throw new ArgumentNullException("Exception in find",a);
+                Console.WriteLine("ArrayError triggered at "+i+". You deal with it.");
+                throw new ArgumentNullException("Need to replace this with a unique excpetion");
+            }
+            finally
+            {
+                Console.WriteLine("Message that prints regardless from find.");
+            }
+        }
+        Console.WriteLine("Leaving find");
     }
 }
