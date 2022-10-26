6db3542fbd2e1baf8e138df5f3242383b35c35b9
SkylerOB <skyler017@revature.net>
2022-10-26 13:49:58 -0700

    array index errors introduced

--- throwcatch/program.cs
+++ throwcatch/program.cs
@@ -13,21 +13,65 @@ public class Program
         // throw errors in subroutines, catch in main
 		Program ThCa = new Program();
 
-        /*
+        // handle the exception where it occurs
+        ThCa.handledException();
+        // nothing to do here
+
+
+        // Let the exception occur and deal with it here.
         try
         {
-            ArrayError();
+           ThCa.unhandledException();
         }
         catch (IndexOutOfRangeException a)
         {
-
+            Console.WriteLine("ArrayError triggered-" + a);
         }
-        */
+        finally
+        {
+            Console.WriteLine("Message that prints regardless.");
+        }
         
+        
+        Console.WriteLine("End of Program. Exiting.");
 
     }
     
-    void ArrayError() throws IndexOutOfRangeException  {
-        
+    void handledException()
+    {
+        int[] sequence = new int[4];
+        int digit = sequence.Length;
+        Console.WriteLine("Array size is: " + digit);
+        Console.WriteLine("There cannot be an element at index " + digit);
+
+        for(int i=0; i<=digit; i++)
+        {
+            try
+            {
+                sequence[i] = i;
+                Console.WriteLine(i + ": " + sequence[i] + "     Nice");
+            }
+            catch (IndexOutOfRangeException a)
+            {
+                Console.WriteLine("ArrayError triggered at " + i + ":" + a);
+            }
+            finally
+            {
+                Console.WriteLine("Message that prints regardless.");
+            }
+        }
+    }
+
+    void unhandledException()
+    {
+        int[] sequence = new int[5];
+        int digit = sequence.Length;
+        Console.WriteLine("Array size is: " + digit);
+        Console.WriteLine("There cannot be an element at index " + digit);
+        for(int i=0; i<=digit; i++)
+        {
+            sequence[i] = i;
+            Console.WriteLine(i + ": " + sequence[i]);
+        }
     }
 }
