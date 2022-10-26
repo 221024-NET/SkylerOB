72e8e9795d4e3f9a35359aa9e7923024fa698894
SkylerOB <skyler017@revature.net>
2022-10-26 13:06:36 -0700

    main created

--- throwcatch/program.cs
+++ throwcatch/program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+	// Fields
+	
+	// Constructor
+
+	// Methods
+	// [access modifier] [modifier] [retu type] [method name] ([parameters])
+	public static void Main()
+	{
+        // throw errors in subroutines, catch in main
+		Program ThCa = new Program();
+
+        /*
+        try
+        {
+            ArrayError();
+        }
+        catch (IndexOutOfRangeException a)
+        {
+
+        }
+        */
+        
+
+    }
+    
+    void ArrayError() throws IndexOutOfRangeException  {
+        
+    }
+}
